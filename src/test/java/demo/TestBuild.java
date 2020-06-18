1ca685dbf049baedc3812dd165ebcba1cadb3e67
mayank <mayankkgarg0@gmail.com>
2020-06-19 00:20:02 +0530

    update from NY

--- src/test/java/demo/TestBuild.java
+++ src/test/java/demo/TestBuild.java
@@ -10,5 +10,8 @@ public class TestBuild {
 		System.out.println("Execute Login Test"); 
 		System.out.println("Execute Login Test1"); 
 		System.out.println("Execute Login Test2"); 
+		//update from NY
+		System.out.println("Execute Login Test2a"); 
+		System.out.println("Execute Login Test2b"); 
 	}
 }
